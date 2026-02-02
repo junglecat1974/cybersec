fe7de868a17cd061fdea2cca508960d7b53d8268
junglecat1974 <taddeigianluca74@gmail.com>
2026-02-02 10:52:13 +0100

    Add files via upload

---
+++ parlo.py
@@ -0,0 +1,13 @@
+import socket 
+import sys
+messaggio = sys.argv[1]
+s = socket.socket()
+s.settimeout(2)
+# indirizzo 10.0.4.254
+indirizzo = '127.0.0.1' 
+porta = 6364
+s.connect((indirizzo,porta))
+s.sendall(messaggio.encode())
+risposta = s.recv(1024)
+print(risposta.decode())
+s.close()
