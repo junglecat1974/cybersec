f94595e8fd21ce3d5469a28e255ba585c391506d
junglecat1974 <taddeigianluca74@gmail.com>
2026-02-02 10:16:26 +0100

    Add files via upload

--- leggo.py
+++ leggo.py
@@ -1,11 +1,19 @@
-## cybersec
-nomefile = "logfile.txt"
-try :
-    f = open (nomefile,"r",encoding='utf-8')
-    righe = f.readlines()
-    for riga in righe:
-        print(riga,end='')
-except FileNotFoundError as e:
-    print (f"[-] errore bloccante: {str(e)}")
-    
+def main():
 
+
+
+    ## cybersec
+    nomefile ="logfile.txt"
+    
+ 
+    try :
+        f = open (nomefile,"r",encoding='utf-8')
+        righe = f.readlines()
+        for riga in righe:
+            print(riga,end='')
+    except FileNotFoundError as e:
+        print (f"[-] errore bloccante: {str(e)}")
+
+
+if __name__=="__main__":
+    main()
